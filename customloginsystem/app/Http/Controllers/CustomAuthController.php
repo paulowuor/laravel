c42df75d859eb55d84e955a4b4f562055df40f51
paulowuor <owuorpaul136@gmail.com>
2023-03-07 17:21:51 +0300

    laravel.h

---
+++ customloginsystem/app/Http/Controllers/CustomAuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CustomAuthController extends Controller
+{
+public function login(){
+return "login";
+}
+public function registration(){
+return "registration";
+}
+}
